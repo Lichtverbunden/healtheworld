9d0391aed29f23da567fa4291705bb4af501a34b
Ken Krippeler <kriken19@gmail.com>
2016-11-23 21:08:12 +0100

    Initial Commit

---
+++ HealTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealTheWorld
+//
+//  Created by Ken Krippeler on 23.11.16.
+//  Copyright © 2016 Lichtverbunden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
